c70d7fd08cf836e63e6814e0e10fabf368bb7a22
Rocky Chowdhury <rocky20809@gmail.com>
2023-06-18 02:55:12 +0600

    new

---
+++ D_Linked_List/Delete.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+void delete_head(Node *&head) {
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
---
+++ D_Linked_List/Insert.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+void insert_head(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    if (tail == NULL)
+    {
+        tail = newNode;
+        head = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+}
+void insert_pos(Node *head, int pos, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for (int i = 1; i < pos; i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    newNode->next->prev = newNode;
+    newNode->prev = tmp;
+}
+void print(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+}
+void print_reverse(Node *tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+    cout << endl;
+}
+int size(Node *head)
+{
+    Node *tmp = head;
+    int count = 0;
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+}
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val, pos;
+    while (true)
+    {
+        cin >> pos >> val;
+        if (val == -1)
+            break;
+        if (pos == 0)
+            insert_head(head, tail, val);
+        else if (pos == size(head))
+            insert_tail(head, tail, val);
+        else if (pos < size(head))
+            insert_pos(head, pos, val);
+        else
+            cout << "ERROR!!!" << endl;
+    }
+    print(head);
+    print_reverse(tail);
+    
+}
