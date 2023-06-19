1f3cf84713093dffd185d855e3666835b310c7e4
Rocky Chowdhury <rocky20809@gmail.com>
2023-06-19 19:47:25 +0600

    new

--- D_Linked_List/Delete.cpp
+++ D_Linked_List/Delete.cpp
@@ -14,8 +14,41 @@ public:
         prev = NULL;
     }
 };
-void delete_head(Node *&head) {
-    
+void delete_at_position(Node *head, int pos)
+{
+    Node *tmp = head;
+    for (int i = 1; i <= pos - 1; i++)
+    {
+        tmp = tmp->next;
+    }
+    Node *deleteNode = tmp->next;
+    tmp->next = tmp->next->next;
+    tmp->next->prev = tmp;
+    delete deleteNode;
+}
+void delete_tail(Node *&head, Node *&tail)
+{
+    Node *deleteNode = tail;
+    tail = tail->prev;
+    delete deleteNode;
+    if (tail == NULL)
+    {
+        head = NULL;
+        return;
+    }
+    tail->next = NULL;
+}
+void delete_head(Node *&head, Node *&tail)
+{
+    Node *deleteNode = head;
+    head = head->next;
+    delete deleteNode;
+    if (head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
 }
 int main()
 {
